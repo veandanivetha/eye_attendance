be68ba641d5c8f0c145f57c8ff02b970e221c206
veandanivetha <nivetha212005@gmail.com>
2025-03-13 21:13:31 +0530

    Add files via upload

---
+++ eye_attendance/face-detection/attendance/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Student, Attendance
+
+admin.site.register(Student)
+admin.site.register(Attendance)
+# Register your models here.
---
+++ eye_attendance/face-detection/attendance/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    roll_no = models.CharField(max_length=20, unique=True)
+    phone_number = models.CharField(max_length=15)
+    parent_contact = models.CharField(max_length=15)
+    face_photo = models.ImageField(upload_to='faces/')
+
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=[('Present', 'Present'), ('Absent', 'Absent')])
